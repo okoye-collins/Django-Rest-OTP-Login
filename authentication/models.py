3cf2eaa201a6df55b6cc4d4743d05dbc10ca32e4
okoye_collins <collins.kcj@gmail.com>
2024-01-10 12:31:52 +0100

    done with the user model

--- authentication/models.py
+++ authentication/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-# class User(AbstractUser):
-#     email = models.EmailField()
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    otp = models.CharField(max_length=6, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.username
--- otp_core/settings.py
+++ otp_core/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "authentication.User"
 
 # Application definition
 
@@ -40,7 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # third party apps
     "rest_framework",
-    "otp",
+    # 'otp',
+    'django_otp',
+    'django_otp.plugins.otp_totp',
 
     # my apps
     "authentication",
